a10771207c26447a2723412c48ef9fdcd56de2bf
Galina Al Sheikhakhmad <galyunya123@gmail.com>
2018-04-04 20:37:11 +0300

    modified take a photo

--- app/src/main/java/com/example/user/portfolio/ChoosePhotoActivity.java
+++ app/src/main/java/com/example/user/portfolio/ChoosePhotoActivity.java
@@ -1,25 +1,21 @@
 package com.example.user.portfolio;
 
-import android.content.ActivityNotFoundException;
+import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.LinearLayout;
 
 public class ChoosePhotoActivity extends AppCompatActivity implements View.OnClickListener {
 
-    public ImageView imageView;
-    final int CAMERA_CAPTURE = 1;
-    final int PIC_CROP = 2;
-    private Uri picUri;
+    public static final String TAKE_RESULT = "take result";
+    public static final int TAKE_PHOTO = 1;
+    public static final int CHOOSE_PHOTO = 2;
     private Button btn_takePhoto;
     private Button btn_choosePhoto;
+    private View closeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +24,7 @@ public class ChoosePhotoActivity extends AppCompatActivity implements View.OnCli
 
         btn_takePhoto = (Button) findViewById(R.id.takePhoto);
         btn_choosePhoto = (Button) findViewById(R.id.choosePhoto);
+        closeView = (View) findViewById(R.id.close_view);
 
     }
 
@@ -36,6 +33,7 @@ public class ChoosePhotoActivity extends AppCompatActivity implements View.OnCli
         super.onStart();
         btn_takePhoto.setOnClickListener(this);
         btn_choosePhoto.setOnClickListener(this);
+        closeView.setOnClickListener(this);
     }
 
     @Override
@@ -43,70 +41,27 @@ public class ChoosePhotoActivity extends AppCompatActivity implements View.OnCli
         super.onStop();
         btn_takePhoto.setOnClickListener(null);
         btn_choosePhoto.setOnClickListener(null);
+        closeView.setOnClickListener(null);
     }
 
     @Override
     public void onClick(View view) {
 
+        Intent intent = new Intent();
         switch (view.getId()) {
             case R.id.takePhoto:
-                try {
-                    // Намерение для запуска камеры
-                    Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(captureIntent, CAMERA_CAPTURE);
-                } catch (ActivityNotFoundException e) {
-                    // Выводим сообщение об ошибке
-                    String errorMessage = "Ваше устройство не поддерживает съемку";
-                    Toast toast = Toast
-                            .makeText(this, errorMessage, Toast.LENGTH_SHORT);
-                    toast.show();
-                }
+                intent.putExtra(TAKE_RESULT, TAKE_PHOTO);
+                setResult(Activity.RESULT_OK, intent);
+                finish();
                 break;
             case R.id.choosePhoto:
-
+                intent.putExtra(TAKE_RESULT, CHOOSE_PHOTO);
+                setResult(Activity.RESULT_OK, intent);
+                finish();
                 break;
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
-            // Вернулись от приложения Камера
-            if (requestCode == CAMERA_CAPTURE) {
-                // Получим Uri снимка
-                picUri = data.getData();
-                // кадрируем его
-                performCrop();
-            }
-            // Вернулись из операции кадрирования
-            else if (requestCode == PIC_CROP) {
-                Bundle extras = data.getExtras();
-                // Получим кадрированное изображение
-                Bitmap thePic = extras.getParcelable("data");
-                // передаём его в ImageView
-                ImageView picView = (ImageView) findViewById(R.id.photo);
-                picView.setImageBitmap(thePic);
-            }
-        }
-    }
-
-    private void performCrop() {
-        try {
-            // Намерение для кадрирования. Не все устройства поддерживают его
-            Intent cropIntent = new Intent("com.android.camera.action.CROP");
-            cropIntent.setDataAndType(picUri, "image/*");
-            cropIntent.putExtra("crop", "true");
-            cropIntent.putExtra("aspectX", 1);
-            cropIntent.putExtra("aspectY", 1);
-            cropIntent.putExtra("outputX", 256);
-            cropIntent.putExtra("outputY", 256);
-            cropIntent.putExtra("return-data", true);
-            startActivityForResult(cropIntent, PIC_CROP);
-        } catch (ActivityNotFoundException anfe) {
-            String errorMessage = "Извините, но ваше устройство не поддерживает кадрирование";
-            Toast toast = Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT);
-            toast.show();
+            case R.id.close_view:
+                finish();
+                break;
         }
     }
 }
--- app/src/main/java/com/example/user/portfolio/MainActivity.java
+++ app/src/main/java/com/example/user/portfolio/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.user.portfolio;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -9,7 +12,9 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    public static ImageView imageView;
+    private ImageView imageView;
+    public static final int BUTTONS_REQUEST_CODE = 1;
+    public static final int CAMERA_REQUEST_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +45,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()){
             case R.id.photo:
                 Intent intent = new Intent(this, ChoosePhotoActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent, BUTTONS_REQUEST_CODE);
                 break;
         }
     }
@@ -49,5 +54,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
+        if(requestCode == BUTTONS_REQUEST_CODE){
+            if(resultCode == Activity.RESULT_OK){
+                int result = data.getIntExtra(ChoosePhotoActivity.TAKE_RESULT, 0);
+                if(result == ChoosePhotoActivity.TAKE_PHOTO){
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, CAMERA_REQUEST_CODE);
+                }
+                if(result == ChoosePhotoActivity.CHOOSE_PHOTO){
+
+                }
+            }
+        }
+
+        if(requestCode == CAMERA_REQUEST_CODE){
+            if(resultCode == Activity.RESULT_OK){
+                Bitmap imageBitmap = (Bitmap) data.getExtras().get("data");
+                imageView.setImageBitmap(imageBitmap);
+            }
+        }
     }
 }
